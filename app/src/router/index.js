4995fb5fb34818c7d3b83304f812b81b439a4ad9
Joe Karanja <87515353+joe052@users.noreply.github.com>
2023-12-12 14:28:50 +0300

    Initial commit

---
+++ app/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+/**Define routes here */
+import HomeView from "../views/HomeView.vue";
+import PostsView from "../views/PostsView.vue";
+import DetailsView from "../views/DetailsView.vue";
+import JobsComponent from "../components/JobsComponent.vue";
+import JobDetailsComponent from "../components/JobDetails.vue";
+
+const routes = [
+  { path: "/", name: "posts", component: PostsView },
+  { path: "/home", name: "home", component: HomeView },
+  { path: "/jobs", name: "jobs", component: JobsComponent },
+  { path: "/posts/:id", name: "details", component: DetailsView,props: true },
+  { path: "/jobs/:id", name: "job_details", component: JobDetailsComponent },
+];
+
+/**Initialize here */
+const router = createRouter({ history: createWebHistory(), routes });
+
+export default router;
