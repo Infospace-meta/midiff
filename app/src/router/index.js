6f8ebade59ed0f9df138c10ad2e9c36c9bc9c41b
joe052 <joekaranjasenior52@gmail.com>
2023-12-27 18:35:58 +0300

    message

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -10,6 +10,7 @@ import JobDetailsComponent from "../components/JobDetails.vue";
 const routes = [
   { path: "/", name: "posts", component: PostsView },
   { path: "/home", name: "home", component: HomeView },
+  { path: "/about", name: "about", component: AboutView },
   { path: "/jobs", name: "jobs", component: JobsComponent },
   { path: "/posts/:id", name: "details", component: DetailsView,props: true },
   { path: "/jobs/:id", name: "job_details", component: JobDetailsComponent },
