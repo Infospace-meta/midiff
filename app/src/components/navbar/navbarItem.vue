83defa45f0bbfbad245a6dc8b1673b65fa237e1d
joe052 <joekaranjasenior52@gmail.com>
2024-01-02 22:27:35 +0300

    message

--- app/src/components/navbar/navbarItem.vue
+++ app/src/components/navbar/navbarItem.vue
@@ -1,4 +1,53 @@
 <template>
+  <nav
+    :class="{ hidden: close, 'sm:max-lg:flex': open }"
+    class="sm:max-lg:z-20 flex flex-col lg:flex lg:flex-row sm:max-lg:w-4/5 absolute lg:relative lg:top-auto top-24 right-0 left-16 h-screen md:h-auto bg-white"
+    id="nav-content"
+  >
+    <!-- <NavAboutUS /> -->
+    <NavbarMenu />
+    <!-- <NavCourses /> -->
+    <!-- <NavBlogs /> -->
+    <!-- <NavContact /> -->
+  </nav>
+  <div
+    class="flex items-center right-0 h-full pl-4 ml-6 border-l-0 lg:border-l border-orange-700"
+  >
+    <button
+      data-collapse-toggle="navbar-sticky"
+      type="button"
+      class="text-sm pl-2 text-white rounded-lg lg:hidden focus:outline-none"
+      aria-controls="navbar-sticky"
+      aria-expanded="false"
+      @click="toggleMenu"
+    >
+      <span class="sr-only">Open main menu</span>
+      <h1 class="text-xl mr-4 text-gray-900 hover:text-blue-700">Menu</h1>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const close = ref(true);
+const open = ref(false); // Set open to false initially
+
+const toggleMenu = () => {
+  const navContent = document.getElementById("nav-content");
+  close.value = !close.value;
+  open.value = !open.value;
+};
+
+// Import your components here
+// import NavAboutUS from "./navLinks/navAboutUS.vue";
+// import NavContact from "./navLinks/navContact.vue";
+// import NavCourses from "./navLinks/navCourses.vue";
+// import NavBlogs from "./navLinks/navBlogs.vue";
+import NavbarMenu from "../navbar/navbarMenu.vue";
+</script>
+
+<!-- <template>
   <div>
     <nav
       :class="{ hidden: close, 'sm:flex': open }"
@@ -61,4 +110,4 @@ const toggleMenu = () => {
   close.value = !close.value;
   open.value = !open.value;
 };
-</script>
+</script> -->
