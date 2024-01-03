645cdbd21aa7dd0d569affdb527a34728467dcdb
immanuel kimani <immanuel4082@gmail.com>
2024-01-04 00:14:31 +0300

    message

--- app/src/main.js
+++ app/src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from "vue";
 import router from "./router";
+import VueSplide from '@splidejs/vue-splide';
 import App from "./App.vue";
 
 /**styling */
 // import "./style.css";
 import "./assets/tailwind.css"; //tailwind
-import { library } from '@fortawesome/fontawesome-svg-core'
+import '@splidejs/vue-splide/css';
+//import '@splidejs/vue-splide/css/core';
+
 
 const app = createApp(App);
 app.use(router);
+app.use(VueSplide);
 app.mount("#app");
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <template>
   <h1 class="text-red-700">Home</h1>
+  <Blogs />
+  <Partners />
 </template>
 
-<script setup></script>
+<script setup>
+import Blogs from '../components/homePage/blogs.vue';
+import Partners from '../components/homePage/partners.vue';
+</script>
 
 <style lang="scss" scoped></style>
