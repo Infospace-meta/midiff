ef881aa4e20df6d09b3640d175e737e4358120d0
immanuel kimani <immanuel4082@gmail.com>
2024-01-03 20:59:35 +0300

    message

--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,11 +1,15 @@
 <template>
   <Hero />
   <AboutUS />
+  <Vision />
+  <Faqs />
 </template>
 
 <script setup>
 import AboutUS from "../components/aboutUs/aboutUS.vue";
 import Hero from "../components/aboutUs/hero.vue";
+import Vision from "../components/aboutUs/vision.vue";
+import Faqs from "../components/aboutUs/faqs.vue";
 </script>
 
 <style lang="scss" scoped></style>
