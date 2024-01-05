70e7620591620d737bfb4ec6f9867e0e5c6ba049
immanuel kimani <immanuel4082@gmail.com>
2024-01-05 14:29:18 +0300

    message

---
+++ app/src/components/blogs/blogPage.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="p-5 mx-auto sm:p-10 md:p-16 dark:bg-gray-100 dark:text-gray-900">
+    <div class="flex flex-col max-w-4xl mx-auto overflow-hidden rounded">
+      <img
+        src="https://source.unsplash.com/random/480x360"
+        alt=""
+        class="w-full h-60 sm:h-96 dark:bg-gray-500"
+      />
+      <div
+        class="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-3xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-100"
+      >
+        <div class="space-y-2">
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            class="inline-block text-2xl font-semibold sm:text-3xl"
+            >The Best Activewear from the Nordstrom Anniversary Sale</a
+          >
+          <p class="text-xs dark:text-gray-800">
+            By
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              class="text-xs hover:underline"
+              >Leroy Jenkins</a
+            >
+          </p>
+        </div>
+        <div class="dark:text-gray-900">
+          <p>
+            <!-- Introduction or summary goes here -->
+
+            <!-- Section 1 -->
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque id felis eget massa ornare rhoncus. Nam lobortis magna
+            ac elementum interdum. Mauris accumsan nisi a efficitur consequat.
+            Maecenas iaculis augue nec dolor blandit semper. Quisque fringilla
+            suscipit magna. Cras felis libero, auctor et purus nec, condimentum
+            rutrum purus.<br /><br />
+
+            <!-- Section 2 -->
+            Curabitur ac odio in felis sagittis egestas ac a neque. Phasellus
+            risus justo, consectetur et vestibulum eget, varius id nulla. In
+            volutpat sem lectus, quis rutrum diam vestibulum nec. Proin bibendum
+            blandit ipsum vitae hendrerit. Mauris odio nibh, ultrices a sapien
+            quis, commodo mollis ante.<br /><br />
+
+            <!-- Section 3 -->
+            Mauris porttitor nisi vitae urna feugiat, sit amet volutpat lacus
+            finibus. Donec non lectus placerat, sollicitudin massa quis,
+            ullamcorper metus. Maecenas mattis urna non blandit gravida. Mauris
+            vulputate gravida sem a malesuada. Maecenas vitae nunc gravida,
+            rhoncus leo vitae, sagittis ante.<br /><br />
+            <!-- Section 1 -->
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque id felis eget massa ornare rhoncus. Nam lobortis magna
+            ac elementum interdum. Mauris accumsan nisi a efficitur consequat.
+            Maecenas iaculis augue nec dolor blandit semper. Quisque fringilla
+            suscipit magna. Cras felis libero, auctor et purus nec, condimentum
+            rutrum purus.<br /><br />
+
+            <!-- Section 2 -->
+            Curabitur ac odio in felis sagittis egestas ac a neque. Phasellus
+            risus justo, consectetur et vestibulum eget, varius id nulla. In
+            volutpat sem lectus, quis rutrum diam vestibulum nec. Proin bibendum
+            blandit ipsum vitae hendrerit. Mauris odio nibh, ultrices a sapien
+            quis, commodo mollis ante.<br /><br />
+
+            <!-- Section 3 -->
+            Mauris porttitor nisi vitae urna feugiat, sit amet volutpat lacus
+            finibus. Donec non lectus placerat, sollicitudin massa quis,
+            ullamcorper metus. Maecenas mattis urna non blandit gravida. Mauris
+            vulputate gravida sem a malesuada. Maecenas vitae nunc gravida,
+            rhoncus leo vitae, sagittis ante.<br /><br />
+            <!-- Section 1 -->
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque id felis eget massa ornare rhoncus. Nam lobortis magna
+            ac elementum interdum. Mauris accumsan nisi a efficitur consequat.
+            Maecenas iaculis augue nec dolor blandit semper. Quisque fringilla
+            suscipit magna. Cras felis libero, auctor et purus nec, condimentum
+            rutrum purus.<br /><br />
+            <img
+              src="https://source.unsplash.com/random/480x360"
+              alt=""
+              class="w-full h-60 sm:h-96 dark:bg-gray-500"
+            /><br /><br />
+
+            <!-- Section 2 -->
+            Curabitur ac odio in felis sagittis egestas ac a neque. Phasellus
+            risus justo, consectetur et vestibulum eget, varius id nulla. In
+            volutpat sem lectus, quis rutrum diam vestibulum nec. Proin bibendum
+            blandit ipsum vitae hendrerit. Mauris odio nibh, ultrices a sapien
+            quis, commodo mollis ante.<br /><br />
+
+            <!-- Section 3 -->
+            Mauris porttitor nisi vitae urna feugiat, sit amet volutpat lacus
+            finibus. Donec non lectus placerat, sollicitudin massa quis,
+            ullamcorper metus. Maecenas mattis urna non blandit gravida. Mauris
+            vulputate gravida sem a malesuada. Maecenas vitae nunc gravida,
+            rhoncus leo vitae, sagittis ante.<br /><br />
+            <!-- Section 1 -->
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque id felis eget massa ornare rhoncus. Nam lobortis magna
+            ac elementum interdum. Mauris accumsan nisi a efficitur consequat.
+            Maecenas iaculis augue nec dolor blandit semper. Quisque fringilla
+            suscipit magna. Cras felis libero, auctor et purus nec, condimentum
+            rutrum purus.<br /><br />
+
+            <!-- Section 2 -->
+            Curabitur ac odio in felis sagittis egestas ac a neque. Phasellus
+            risus justo, consectetur et vestibulum eget, varius id nulla. In
+            volutpat sem lectus, quis rutrum diam vestibulum nec. Proin bibendum
+            blandit ipsum vitae hendrerit. Mauris odio nibh, ultrices a sapien
+            quis, commodo mollis ante.<br /><br />
+
+            <!-- Section 3 -->
+            Mauris porttitor nisi vitae urna feugiat, sit amet volutpat lacus
+            finibus. Donec non lectus placerat, sollicitudin massa quis,
+            ullamcorper metus. Maecenas mattis urna non blandit gravida. Mauris
+            vulputate gravida sem a malesuada. Maecenas vitae nunc gravida,
+            rhoncus leo vitae, sagittis ante.<br /><br />
+            <!-- Section 1 -->
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque id felis eget massa ornare rhoncus. Nam lobortis magna
+            ac elementum interdum. Mauris accumsan nisi a efficitur consequat.
+            Maecenas iaculis augue nec dolor blandit semper. Quisque fringilla
+            suscipit magna. Cras felis libero, auctor et purus nec, condimentum
+            rutrum purus.<br /><br />
+
+            <!-- Section 2 -->
+            Curabitur ac odio in felis sagittis egestas ac a neque. Phasellus
+            risus justo, consectetur et vestibulum eget, varius id nulla. In
+            volutpat sem lectus, quis rutrum diam vestibulum nec. Proin bibendum
+            blandit ipsum vitae hendrerit. Mauris odio nibh, ultrices a sapien
+            quis, commodo mollis ante.
+
+            <!-- ... Add more sections as needed -->
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
