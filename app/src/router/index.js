9ab6550abba89ee7f4bb2c832cbc85a4d433f783
joe052 <joekaranjasenior52@gmail.com>
2024-02-10 19:27:55 +0300

    message

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -2,10 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 
 /**Import components here */
 import HomeView from "../views/HomeView.vue";
-import PostsView from "../views/PostsView.vue";
-import DetailsView from "../views/DetailsView.vue";
-import JobsComponent from "../components/JobsComponent.vue";
-import JobDetailsComponent from "../components/JobDetails.vue";
 import AboutView from "../views/AboutView.vue";
 import CoursesView from "../views/CoursesView.vue";
 import DeptView from "../views/DeptView.vue";
@@ -19,9 +15,6 @@ const routes = [
   { path: "/courses", name: "courses", component: CoursesView },
   { path: "/departments", name: "departments", component: DeptView },
   { path: "/news", name: "news", component: NewsView },
-  { path: "/jobs", name: "jobs", component: JobsComponent },
-  { path: "/posts/:id", name: "article", component: DetailsView, props: true },
-  { path: "/jobs/:id", name: "job_details", component: JobDetailsComponent },
 ];
 
 /**Initialize here */
