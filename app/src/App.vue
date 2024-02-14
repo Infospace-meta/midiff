7a664ee6ea4762c607c93d9c8f1fc22dc5b8f251
joe052 <joekaranjasenior52@gmail.com>
2024-02-15 02:56:42 +0300

    message

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,3 +1,30 @@
+<template>
+  <div class="min-h-screen bg-white">
+    <ContactNav />
+    <Header :isFixed="isHeaderFixed" />
+    <div class="" @scroll="handleScroll">
+      <RouterView />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { RouterView } from "vue-router";
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
+import ContactNav from "./components/navbar/navcontact.vue";
+
+const isHeaderFixed = ref(false);
+
+const handleScroll = () => {
+  isHeaderFixed.value = window.scrollY > 0;
+};
+
+window.addEventListener("scroll", handleScroll);
+</script>
+
 <!-- <template>
   <div class="min-h-screen bg-white">
     <Header />
@@ -15,30 +42,3 @@ import Footer from "./components/Footer.vue";
 </script>
 
 <style scoped></style> -->
-
-<template>
-  <div class="min-h-screen bg-white">
-    <ContactNav />
-    <ImmanuelHeader :isFixed="isHeaderFixed" />
-    <div class="" @scroll="handleScroll">
-      <RouterView />
-    </div>
-    <Footer />
-  </div>
-</template>
-
-<script setup>
-import { ref } from "vue";
-import { RouterView } from "vue-router";
-import Footer from "./components/Footer.vue";
-import ImmanuelHeader from "./components/ImmanuelHeader.vue";
-import ContactNav from "./components/navbar/navcontact.vue";
-
-const isHeaderFixed = ref(false);
-
-const handleScroll = () => {
-  isHeaderFixed.value = window.scrollY > 0;
-};
-
-window.addEventListener("scroll", handleScroll);
-</script>
