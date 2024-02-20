e6ba6edba256bed46d4aaa558d4a922eabdb88f1
immanuel kimani <immanuel4082@gmail.com>
2024-02-20 20:14:40 +0300

    message

--- app/src/components/info/studentLife.vue
+++ app/src/components/info/studentLife.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hero-section max-w-7xl mx-auto p-4">
+  <div class="hero-section max-w-7xl mx-auto">
     <div class="">
       <img
         src="../../assets/students.jpg"
@@ -7,33 +7,80 @@
         class="lg:w-full lg:h-[500px] object-cover object-top"
       />
     </div>
-    <div
-      class="bg-gray-100 p-4 lg:pt-10 lg:pb-20 lg:-translate-y-32 lg:translate-x-20 lg:w-1/2"
-    >
-      <h2 class="text-2xl lg:text-5xl capitalize text-center">
-        Student College Life
-      </h2>
+    <div class="flex">
+      <div
+        class="bg-gray-200 p-4 lg:pt-10 lg:pb-10 lg:-translate-y-32 lg:w-1/2"
+      >
+        <h2
+          class="text-2xl lg:text-5xl capitalize border-b-2 p-4 border-gray-400"
+        >
+          Student College Life
+        </h2>
+      </div>
+      <div
+        class="bg-gray-800 text-white p-10 lg:p-10 lg:w-2/3 z-10 lg:-translate-y-5"
+      >
+        <h1
+          class="lg:text-4xl text-xl capitalize lg:pb-4 flex justify-center items-center gap-3"
+        >
+          Welcome Home
+          <hr class="w-2/4" />
+        </h1>
+        <p class="lg:text-lg">
+          From your lectures and House advisers to your friends and teammates,
+          Midiff is a vibrant community where you can feel supported and
+          encouraged. No matter where you come from, Midiff is home.
+        </p>
+      </div>
     </div>
-    <div
-      class="bg-purple-700 text-white p-10 lg:p-10 lg:w-1/2 lg:translate-x-[500px] lg:-translate-y-48"
-    >
-      <h1 class="lg:text-4xl text-xl underline capitalize lg:pb-4">Welcome Home</h1>
-      <p class="lg:text-lg">
-        From your lectures and House advisers to your friends and teammates,
-        Midiff is a vibrant community where you can feel supported and
-        encouraged. No matter where you come from, Midiff is home.
+  </div>
+  <div class="max-w-7xl mx-auto lg:flex lg:-translate-y-4">
+    <div class="lg:w-1/2 bg-gray-200 lg:-translate-y-28">
+      <p class="text-2xl text-gray-700 p-14 text-center lg:-translate-x-8">
+        Student life at Midiff Institute of Professional Studies is vibrant,
+        dynamic, and filled with opportunities for growth and exploration. As a
+        leading vocational education and training institution, Midiff Institute
+        is dedicated to providing a nurturing and enriching environment where
+        students can thrive academically, socially, and personally.
       </p>
     </div>
-  </div>
-  <div class="">
-    <div></div>
-    <div></div>
+
     <div></div>
   </div>
-  <div class="Accomodation">
+
+  <div
+    class="Accomodation bg-gray-800 max-w-7xl mx-auto lg:flex pb-64 lg:-translate-y-36"
+  >
+    <div class="p-10 lg:w-1/2 lg:translate-x-28 z-10">
+      <img
+        src="../../assets/hostel.jpg"
+        alt=""
+        class="lg:w-full lg:h-[370px] object-cover object-top"
+      />
+    </div>
+    <div class="lg:w-2/3">
+      <h1 class="text-7xl lg:translate-y-36 lg:translate-x-56 text-white">Hostels</h1>
+      <hr class="translate-y-56" />
+      <h2
+        class="text-2xl text-white p-14 text-center lg:translate-y-64 border-l-2 border-white"
+      >
+        Our campus life extends beyond the classroom, with comfortable and
+        secure hostel accommodations that provide students with a supportive
+        living environment. Whether from near or far, students find a home away
+        from home in our well-appointed hostels, fostering friendships and
+        enriching experiences that enhance their educational journey.
+      </h2>
+    </div>
     <div></div>
-    <div></div>
-    <div></div>
+  </div>
+  <div
+    class="lg:-translate-y-[1100px] lg:translate-x-[640px] max-w-7xl mx-auto"
+  >
+    <img
+      src="../../assets/student2.jpg"
+      alt=""
+      class="lg:w-1/2 lg:h-[370px] object-cover object-top"
+    />
   </div>
 </template>
 <script setup></script>
